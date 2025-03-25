83c54e43bf2d31bd69540519ffca0ba07d36b661
Kristi Tanner <kristi.tanner@gmail.com>
2025-03-25 00:36:38 -0400

    fix mobile hover

--- src/App.js
+++ src/App.js
@@ -312,24 +312,24 @@ const BookPopularityVisualization = () => {
             };
             
             // Dynamic tooltip style
-            const dynamicTooltipStyle = {
-              ...styles.tooltip,
-              ...(hoveredBook === i ? styles.tooltipVisible : {}),
-              ...(isMobile ? {
-                position: 'absolute',
-                left: '0',
-                right: '0',
-                top: '0',
-                bottom: '0',
-                width: '100%',
-                height: '100%',
-                background: 'rgba(0,0,0,0.85)', // Semi-transparent background
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                zIndex: 60
-              }: {})
+const dynamicTooltipStyle = {
+  ...styles.tooltip,
+  ...(hoveredBook === i ? styles.tooltipVisible : {}),
+  ...(isMobile ? {
+    position: 'absolute',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0,0,0,0.85)', // Semi-transparent background
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 60
+  }: {})
             };
             
             // Dynamic title style
