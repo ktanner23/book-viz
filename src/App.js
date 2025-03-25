e5fab67179be584985305648f372c19ddea0d4d0
Kristi Tanner <kristi.tanner@gmail.com>
2025-03-24 22:06:04 -0400

    adjust book

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ const styles = {
     opacity: 0,
     transition: 'all 0.3s ease',
     zIndex: 40,
-    transform: 'translate(-50%, -90%)',
+    transform: 'translate(-50%, -180%)',
     left: '50%',
     top: '-10px'
   },
